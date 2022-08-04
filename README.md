39aa7980b26de6d482e569a40be45496bc908ec1
Tomáš Pavlík <tomas.pavlik@avast.com>
2022-08-05 01:33:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Five(Six) Clique
+
+A solution to the problem of finding five English words with 25 distinct characters, using graph theory.
+
+## Description
+
+This is improvement of [solution from Benjamin Paassen](https://gitlab.com/bpaassen/five_clique) that decreases the time needed further down from ~21m to ~45s.
+
+## Method
+
+My solution uses finding clique in the graph as Benjamin Paassen does. But the improvment is that in every step we only choose words that contain the most uncommon and unused letter. This is possible due to the fact that all letters have to be used at some point except one. I solved this issue by add all single letter words and I am finding six-clique instead of five-clique.
+
+"But Parker gave it a go. And that's something." ~ Benjamin Paassen
+
+## Quickstart guide
+
+1. Download the `words_alpha.txt` file from https://github.com/dwyl/english-words (this is the same file that Parker used).
+3. Run the `savlik_clique.py` file.
---
+++ savlik_clique.py
@@ -0,0 +1,64 @@
+words = []  # list of tuples: (word, charset, neighbours)
+words_that_contain_letter = {}
+
+print('--- reading words file ---')
+# words_alpha.txt from https://github.com/dwyl/english-words
+with open('words_alpha.txt') as f:
+	for word in f:
+		word = word[:-1]
+		if len(word) != 5:
+			continue
+		char_set = set(word)
+		if len(char_set) != 5:
+			continue
+		words.append((word, char_set, set()))
+
+# add single characters as valid words
+for l in "qwertyuiopasdfghjklzxcvbnm":
+	words.append((l, set([l]), set()))
+	words_that_contain_letter[l] = set()
+
+print('--- building neighborhoods ---')
+for i in range(len(words)):
+	for j in range(len(words)):
+		if len(words[i][1] & words[j][1]) == 0:
+			words[i][2].add(j)
+
+print('--- filling words_that_contain_letter ---')
+# fill words_that_contain_letter
+for i, word in enumerate(words):
+	for l in word[0]:
+		words_that_contain_letter[l].add(i)
+
+# select order of letter by the frequency from lowest to highest
+order = [k for k, v in sorted(words_that_contain_letter.items(), key=lambda x: len(x[1]))]
+def first_letter_unused(set_of_letters):
+	for l in order:
+		if l not in set_of_letters:
+			return l
+
+print('--- start clique finding (this will not take that long ;-)) ---')
+# start clique finding
+results = []
+def solve(word_remaining, letters_used, word_candidates, chosen_words):
+	if word_remaining == 0:
+		results.append(chosen_words)
+		return
+	# choose from candidates only words that contain the most uncommon unused letter
+	for i in word_candidates & words_that_contain_letter[first_letter_unused(letters_used)]:
+		word_candidates2 = word_candidates & words[i][2]
+		if len(word_candidates2) < word_remaining-1:
+			continue
+		letters_used2 = letters_used | words[i][1]
+		if len(letters_used2) % 5 >= 2:
+			continue
+		solve(word_remaining-1, letters_used2, word_candidates2, chosen_words+[words[i][0]])
+
+solve(6, set(), set(range(len(words))), [])
+
+print('completed! Found %d results' % len(results))
+
+for c in results:
+	print(c)
+
+print('completed! Found %d results' % len(results))
